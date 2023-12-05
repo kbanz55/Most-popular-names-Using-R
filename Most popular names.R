61c2f65f9975a9bc72262d2a99e332a4001a05e7
Abu kingsley <kingsleyi67@gmail.com>
2023-12-05 15:38:41 +0000

    Update Most popular names.R

--- Most popular names.R
+++ Most popular names.R
@@ -1,12 +1,21 @@
 library(tidyverse)
 install.packages("babynames")
 library(babynames)
+library(dplyr)
+library(ggplot2)
+data("babynames")
 babynames
 view(babynames)
-babynames %>% filter(sex == "M", year >= 1900, year <= 1999) %>%
-  group_by(name) %>% summarize(total_births = sum(n)) %>%
-  top_n(3, total_births) 
-view(babynames %>% filter(sex == "M", year >= 1900, year <= 1999) %>%
-       group_by(name) %>% summarize(total_births = sum(n)) %>%
-       top_n(3, total_births))
+top_male_names <- babynames %>% filter(sex == "M", year >= 1900, year <= 1999) %>%
+  group_by(name) %>% summarize(total_births = sum(n)) %>% top_n(3, total_births) 
 
+top_male_names_data <- babynames %>% filter(sex == "M", year >= 1900, year <= 1999, name %in% 
+           top_male_names$name) 
+
+ggplot(top_male_names_data, aes(x = as.numeric(year), y = n, color = name)) +
+  geom_line(size = 1.5) + labs(
+    title = "Popularity of Top 3 Male Names From (1900-1999)",
+    x = "Year",
+    y = "Number of Births",
+    color = "Name" 
+  )
